b4239ee578982a6eff83a6de7a25d5922f500182
ahmad-ausaf0 <ahmadausaf0@gmail.com>
2024-05-19 13:09:13 +0530

    resume-link

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -24,8 +24,9 @@ export class ResumeComponent implements OnInit {
     const link = this.renderer.createElement('a');
     link.setAttribute('target', '_blank');
     // link.setAttribute('href', 'https://online.publuu.com/487104/1094068');
-    link.setAttribute('href', 'https://publuu.com/flip-book/490690/1103231');
-    link.setAttribute('download', 'AusafResume.pdf');
+    // link.setAttribute('href', 'https://publuu.com/flip-book/490690/1103231');
+    link.setAttribute('href', 'https://drive.google.com/file/d/1ivci83Tzb7JadNMTyPcQ7T_q3ETQW6tp/view?usp=sharing');
+    // link.setAttribute('download', 'AusafResume.pdf');
     link.click();
     link.remove();
   }
